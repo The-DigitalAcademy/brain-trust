bdfc09183d8f9838f44f021bbc36183b48950561
God-freyMoses <godfreymoses203@gmail.com>
2025-06-20 22:04:33 +0200

    swapped Banele's displayResults for mine..then added a few touch-ups with emojis--for aesthetics

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
 
 public class Welcome {
+
+    static String Username;
     public Welcome() {
         showWelcomeMessage();
     }
 
     public static void showWelcomeMessage() {
         System.out.println("\n=================================");
-        System.out.println("Welcome to BrainTrust");
-        System.out.println("Where Knowledge Meets Confidence");
+        System.out.println("Welcome to BrainTrust🧠");
+        System.out.println("Where Knowledge Meets Confidence😎");
         System.out.println("=================================");
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("\nPlease enter your name: ");
         String name = scanner.nextLine();
-        System.out.println("\nWelcome " + name + " to BrainTrust! You can start the quiz.\n");
+        Username = name;
+        System.out.println("\nWelcome🙂 " + name + " to BrainTrust! You can start the quiz.\n");
     }
 }
